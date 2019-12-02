344ca3e78faad254cd809ee4b6a093294a60aa47
Michael Kiros <michaelakiros@gmail.com>
2019-12-02 08:41:51 -0600

    Adds the script for creating a SQL table based on CSV
    The script creates a table of states and the number of crimes.

--- scripts/create-crimes-table.py
+++ scripts/create-crimes-table.py
@@ -2,20 +2,20 @@
 Script used to create the crimes table. The format of the
 table is as follows:
     -----------------------------
-    |   State   |   Crime Rate  |
+    |   State   |   Crime Count |
     -----------------------------
-    |   Kansas  |   4.2         |
+    |   Kansas  |   422         |
     |   ...     |   ...         |
     -----------------------------
 """
 
-from pandas import DataFrame, read_csv
 import pandas as pd
 
 from configparser import ConfigParser
 
+import psycopg2
 
-def config(filename='database.ini', section='postgresql'):
+def configure(filename='../database.ini', section='postgresql'):
     # create a parser
     parser = ConfigParser()
     # read config file
@@ -32,13 +32,61 @@ def config(filename='database.ini', section='postgresql'):
 
     return db
 
+def insert_data(rows):
+    print('* Insert data into table')
+
+    config = configure()
+    conn = psycopg2.connect(**config)
+    cur = conn.cursor()
+    for row in rows:
+        cur.execute("""INSERT into crimes(state, count)
+            VALUES ('{}', {})""".format(row[0], row[1]))
+    print("* Writing changes to database..")
+    conn.commit()
+    cur.close()
+    print("* Successfully completed")
+
+def create_table():
+    commands = (
+        """CREATE TABLE crimes (
+            state VARCHAR(255) PRIMARY KEY,
+            count INTEGER NOT NULL
+        )""",
+    )
+
+    conn = None
+    try:
+        config = configure()
+        conn = psycopg2.connect(**config)
+        cur = conn.cursor()
+
+        for command in commands:
+            print("* Executing query: {}".format(command))
+            cur.execute(command)
+        cur.close()
+        conn.commit()
+    except Exception as error:
+        print(error)
+
 def main():
+    print("* Reading data from crime-by-state.xls")
     df = pd.read_excel('../datasets/crime-by-state.xls')
     df = df.fillna(method='ffill')
     crimes = (df.groupby('State', as_index=False).apply(
         lambda x: x if len(x)==1 else x.iloc[[-2]]).reset_index(level=0, drop=True))
     crimes = crimes[['State', 'Violent crime']]
-    print(crimes)
+    rows = []
+    for excel_row in crimes.values:
+        row = tuple(excel_row)
+        state = row[0].lower()
+        rate = row[1]
+        if state == 'district of columbia3':
+            state = 'district of columbia'
+        rows.append((state, rate))
+
+    print("* Creating table...")
+    create_table()
+    insert_data(rows)
 
 if __name__ == '__main__':
     main()
